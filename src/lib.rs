b4abbc688a8e89c35c360bacb600e5b97d132085
CeeBeeEh <cblasko@gmail.com>
2024-12-27 21:37:04 -0500

    changed Error return types from "dyn Error" to "anyhow" to workaround "unknown size at compile time" issues

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ pub mod bvr_image;
 pub const INTERFACE_VERSION: &str = "0.2.1";
 
 use std::error::Error;
+use anyhow;
 use crate::bvr_detection::BvrDetection;
 use crate::bvr_image::BvrImage;
 
-pub type Result<T, E = dyn Error> = std::result::Result<T, E>;
 pub trait BvrDetector {
-    fn is_running(&self) -> impl std::future::Future<Output = Result<bool>> + Send;
-    fn init_detector(&mut self, model_details: String) -> impl std::future::Future<Output = Result<()>> + Send;
-    fn detect(&self, bvr_image: BvrImage) -> impl std::future::Future<Output = Result<Vec<BvrDetection>>> + Send;
+    fn is_running(&self) -> impl std::future::Future<Output = anyhow<bool>> + Send;
+    fn init_detector(&mut self, model_details: String) -> impl std::future::Future<Output = anyhow<()>> + Send;
+    fn detect(&self, bvr_image: BvrImage) -> impl std::future::Future<Output = anyhow<Vec<BvrDetection>>> + Send;
     fn get_library_device_types(&self) -> Vec<&'static str>;
     fn is_valid_device_type(&self, device_type: String) -> bool;
     fn get_library_processing_types(&self) -> Vec<&'static str>;
